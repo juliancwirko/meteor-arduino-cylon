e2ad87727022a5a4ed62d1d34203de5bdfb84d18
Julian Ćwirko <julian.cwirko@gmail.com>
2015-04-11 11:22:29 +0200

    cylon version bump

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
     name: 'juliancwirko:arduino-cylon',
     summary: 'Meteor and Cylon.js for Arduino',
-    version: '1.0.0',
+    version: '1.1.0',
     git: 'https://github.com/juliancwirko/meteor-arduino-cylon.git'
 });
 
 Npm.depends({
-    'cylon-firmata': '0.19.0'
+    'cylon-firmata': '0.20.0'
 });
 
 Package.onUse(function(api) {
