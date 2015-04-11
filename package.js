258242f6024f781530410ddc8facb4d650f6dabb
Julian Ćwirko <julian.cwirko@gmail.com>
2015-04-11 13:40:26 +0200

    npm deps

--- package.js
+++ package.js
@@ -1,13 +1,16 @@
 Package.describe({
     name: 'juliancwirko:arduino-cylon',
     summary: 'Meteor and Cylon.js for Arduino',
-    version: '1.1.0',
+    version: '1.1.1',
     git: 'https://github.com/juliancwirko/meteor-arduino-cylon.git'
 });
 
 Npm.depends({
     'serialport': '1.3.1',
-    'cylon-firmata': '0.20.0'
+    'cylon': '1.0.0',
+    'cylon-firmata': '0.20.0',
+    'cylon-gpio': '0.23.0',
+    'cylon-i2c': '0.21.0'
 });
 
 Package.onUse(function(api) {
